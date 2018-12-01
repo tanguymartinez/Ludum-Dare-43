620c850d4affd9e4a817a8f46457452488b87c0c
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-12-01 14:28:21 +0100

    Modify networking

--- Game/Scripts/CLI.gd
+++ Game/Scripts/CLI.gd
@@ -4,7 +4,7 @@ const SERVER_PORT = 1337
 const MAX_PLAYERS = 1
 
 # Player info, associate ID to data
-var player_info = {}
+var text = []
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -12,12 +12,20 @@ func _ready():
 	var peer = NetworkedMultiplayerENet.new()
 	peer.create_server(SERVER_PORT, MAX_PLAYERS)
 	get_tree().set_network_peer(peer)
+	print(IP.get_local_addresses())
+	text.append(IP.get_local_addresses()[0])
 
 func _player_connected(id):
 	print("Player connected...") # Will go unused, not useful here
 
 func _player_disconnected(id):
-	player_info.erase(id) # Erase player from info
+	print("Player disconnected...") # Erase player from info
+
+func refresh():
+	var string
+	for line in text:
+		string += line + "\n"
+	$"CLI".text = string
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
