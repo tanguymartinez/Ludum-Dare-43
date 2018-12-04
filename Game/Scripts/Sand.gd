301c1fd8ad136bd7612230f34e25568790982f49
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-12-04 13:16:46 +0100

    Add group inheritance

--- Game/Scripts/Sand.gd
+++ Game/Scripts/Sand.gd
@@ -1,13 +1,9 @@
-extends Sprite
+extends "Tile.gd"
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var group
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	group = Classes.group.new("NonBlocking")
-	print(group.get_group())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
