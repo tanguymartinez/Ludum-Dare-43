805d0aa12cab8446b85d669f8d25e023bdd31c75
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-12-07 16:49:16 +0100

    Fix space triggering ui_accept event

--- Game/Scripts/Button.gd
+++ Game/Scripts/Button.gd
@@ -1,18 +1,5 @@
 extends Container
 
-
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
-
 func _on_Exit_pressed():
 	get_tree().quit()
 
@@ -27,6 +14,3 @@ func _on_Connect_pressed():
 	if $"Address".text.is_valid_ip_address():
 		Variables.IpAddress = $"Address".text
 		get_tree().change_scene("res://Scenes/Visual.tscn")
-
-func _on_Button_pressed():
-	pass # Replace with function body.
